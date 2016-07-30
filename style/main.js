3d87cebcaa1c085e1571d2e6b27605d43994b8c4
erikmura44 <erikmura44@gmail.com>
2016-07-29 21:58:15 -0600

    added input field

---
+++ style/main.js
@@ -0,0 +1,7 @@
+function initMap() {
+        var mapDiv = document.getElementById('map');
+        var map = new google.maps.Map(mapDiv, {
+            center: {lat: 39.742043, lng: -104.991531},
+            zoom: 8
+        });
+      }
