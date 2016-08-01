2346d18914d894fb8f9dfe1d603028ac7d825471
erikmura44 <erikmura44@gmail.com>
2016-08-01 16:11:42 -0600

    Locations work for 1 or multiple pages

--- style/main.js
+++ style/main.js
@@ -1,5 +1,6 @@
 $('.city').change(function(){
    city = $(this).val()
+
    console.log(city)
 })
 $('.state').change(function(){
@@ -8,7 +9,7 @@ $('.state').change(function(){
 })
 $('form').on('submit', function(event){
    event.preventDefault()
-      myUrl = 'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations?locality='+city+'&region='+state+'&key=ee92426cf2e9727bd6730249465b5c54',
+      myUrl = 'https://galvanize-cors-proxy.herokuapp.com/http://api.brewerydb.com/v2/locations?locality='+city+'&region='+state+'&key=ee92426cf2e9727bd6730249465b5c54',
          console.log(myUrl);
          $.get(myUrl)
          .then (function (data){
@@ -17,7 +18,7 @@ $('form').on('submit', function(event){
                   if (data.numberOfPages > 1){
                      numPages = data.numberOfPages;
                         for(i = 2; i <= numPages; i++){
-                           $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations?locality='+city+'&region='+state+'&p='+i+'&key=ee92426cf2e9727bd6730249465b5c54')
+                           $.get('https://galvanize-cors-proxy.herokuapp.com/http://api.brewerydb.com/v2/locations?locality='+city+'&region='+state+'&p='+i+'&key=ee92426cf2e9727bd6730249465b5c54')
                            .then (function (dataTwo){
                               namesArr = namesArr.concat(dataTwo.data);
                               for(i in namesArr){
@@ -41,6 +42,16 @@ $('form').on('submit', function(event){
          for(i in namesArr){
                if (namesArr[i].brewery.name !== ('Main Brewery')){
                   console.log(namesArr[i].brewery.name);
+                  var listItems = $('<li>');
+                  listItems.text(namesArr[i].brewery.name);
+                  listItems.addClass('list-group-item');
+                  listItems.attr('data-website', namesArr[i].brewery.website);
+                  listItems.on('click', function(){
+                     var website = $(this).attr('data-website');
+                     $(location).attr('href', website);
+                  })
+                  $('.list-group').append(listItems);
+
             }
          }
       }
