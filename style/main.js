80adc130fff05be3d59028401fcf65fc45d799c2
erikmura44 <erikmura44@gmail.com>
2016-08-01 14:19:15 -0600

    Added links to output

--- style/main.js
+++ style/main.js
@@ -10,20 +10,29 @@ $('form').on('submit', function(event){
    event.preventDefault()
       myUrl = 'https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations?locality='+city+'&region='+state+'&key=ee92426cf2e9727bd6730249465b5c54',
          console.log(myUrl);
+         $.get(myUrl)
+         .then (function (data){
+            var namesArr = data.data;
+               console.log(data.numberOfPages);
+                  if (data.numberOfPages > 1){
+                     numPages = data.numberOfPages;
+                        for(i = 2; i <= numPages; i++){
+                           $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations?locality='+city+'&region='+state+'&p='+i+'&key=ee92426cf2e9727bd6730249465b5c54')
+                           .then (function (dataTwo){
+                              namesArr = namesArr.concat(dataTwo.data);
+                              for(i in namesArr){
+                                 if (namesArr[i].brewery.name !== ('Main Brewery')){
+                                    console.log(namesArr[i].brewery.name);
 
-$.get(myUrl)
-   .then (function (data){
-      var namesArr = data.data;
-      console.log(data.numberOfPages);
-         if (data.numberOfPages > 1){
-            numPages = data.numberOfPages;
-               for(i = 2; i <= numPages; i++){
-                  $.get('https://cors-anywhere.herokuapp.com/http://api.brewerydb.com/v2/locations?locality='+city+'&region='+state+'&p='+i+'&key=ee92426cf2e9727bd6730249465b5c54')
-                  .then (function (dataTwo){
-                     namesArr = namesArr.concat(dataTwo.data);
-                     for(i in namesArr){
-                        if (namesArr[i].brewery.name !== ('Main Brewery')){
-                           console.log(namesArr[i].brewery.name);
+                                    var listItems = $('<li>')
+                                    listItems.text(namesArr[i].brewery.name);
+                                    listItems.addClass('list-group-item');
+                                    $(listItems).on('click', function(){
+                                       $(location).attr('href', namesArr[i].brewery.website)
+                                    });
+                                    $('.list-group').append(listItems);
+
+
                   }
                }
             })
@@ -37,3 +46,11 @@ $.get(myUrl)
       }
    })
 })
+
+
+
+// var listOfBreweries = $(".list-group")
+// var newList = $("<li>");
+// newList.attr("value", namesArr[i].brewery.name);
+// newList.text(title["namesArr[i].brewery.name"]);
+// listOfBreweries.append(newOptions);
