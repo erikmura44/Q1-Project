d5785e3f452538e03a99df4a7850ddd98e57034f
erikmura44 <erikmura44@gmail.com>
2016-08-08 09:47:34 -0600

    Fixed landing page, updated css

--- style/landing.js
+++ style/landing.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('.pour').delay(1000).animate({height: '360px'}, 1500).delay(1600).slideUp(500);//Pour Me Another Drink, Bartender!
+  $('.pour').delay(1000).animate({height: '380px'}, 1500).delay(1600).slideUp(500);//Pour Me Another Drink, Bartender!
 
   $('#liquid').delay(2400).animate({height: '170px'}, 2500);// I Said Fill 'Er Up!
 
