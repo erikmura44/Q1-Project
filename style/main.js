c91e565b125aa2470ae78228ba88c4be1f8b2de5
erikmura44 <erikmura44@gmail.com>
2016-08-08 16:43:20 -0600

    added quotes

--- style/main.js
+++ style/main.js
@@ -144,15 +144,18 @@ $('#state').focus(function() {
 })
 
 var quotes = [
-   "Beer, it’s the best damn drink in the world.",
-   "There is no such thing as a bad beer. It’s that some taste better than others.",
+   "He was a wise man who invented beer. - Plato",
+   "Beer, if drunk in moderation, softens the temper, cheers the spirit and promotes health. - Thomas Jefferson",
+   "Beer, it’s the best damn drink in the world. - Jack Nicholson",
+   "There is no such thing as a bad beer. It’s that some taste better than others. - Bill Carter",
    "The best beer in the world is the open bottle in your hand - Danny Jansen",
    "Beer is not the answer. Beer is the question. 'Yes' is the answer.",
+   "Beer’s intellectual. What a shame so many idiots drink it. - Ray Bradbury",
    "I work until beer o'clock",
    "Beer is living proof that God loves us an wants us to be happy.",
    "Beer: It doesn't have many vitamins, that's why you need to drink lots of it.",
    "Friends bring happiness into your life, best friends bring beer.",
-   "I got 99 problems & beer solves all of 'em - Earl Dibbles Jr'"
+   "I got 99 problems & beer solves all of 'em - Earl Dibbles Jr"
 ];
 
 function generate() {
